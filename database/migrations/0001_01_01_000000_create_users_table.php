2ebf2782edd8b64f764a4a92af75eccd1037b469
Dardan-Osmani <dardanosmani18@gmail.com>
2024-07-04 14:20:32 +0200

    Removed role_id from user migration, added to other migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('role_id')->constrained()->onDelete('cascade')->onDelete('restrict');
             $table->rememberToken();
             $table->timestamps();
         });
